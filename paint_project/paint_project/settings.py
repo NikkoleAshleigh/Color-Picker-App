f8236381430b4da9e10fee02919e765e0065a804
NikkoleAshleigh <nikkole.hardy.7.1.21@gmail.com>
2022-12-02 17:38:21 -0500

    first commit

--- paint_project/paint_project/settings.py
+++ paint_project/paint_project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'paint_app.apps.PaintAppConfig'
+    'paint_app.apps.PaintAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- paint_project/paint_project/urls.py
+++ paint_project/paint_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('paint/', include('paint_app.urls'))
+    path('paint/', include('paint_app.urls')),
 ]
